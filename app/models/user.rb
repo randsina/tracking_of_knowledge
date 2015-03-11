0fcab699ab09cbd1515c3861187097554805b70d
Roman Vakulchik <roman.vakulchik@gmail.com>
2015-03-11 17:03:14 +0300

    add avatar to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   belongs_to :role
   before_create :set_default_role
   has_and_belongs_to_many :technologies
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50"}, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks",
     registrations: 'users/registrations' }
   resources :users
-  root to: 'visitors#index'
+  root to: 'technologies#index'
 end
