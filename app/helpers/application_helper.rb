29c737a3ef548994a71969025ec5f609e4272476
Roman Vakulchik <roman.vakulchik@gmail.com>
2015-03-27 01:18:34 +0300

    add progress to index page of technology

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,16 @@ module ApplicationHelper
   def progress_theme(user, survey_id)
     survey = Survey::Survey.find(survey_id)
     user_attempts = survey.attempts.for_participant(user)
-    if user_attempts.empty?
-      "0%"
-    else
-      "#{(user_attempts.high_score.fdiv(survey.questions.count) * 100).to_i}%"
+    user_attempts.empty? ? 0 : (user_attempts.high_score.fdiv(survey.questions.count) * 100).to_i
+  end
+
+  def progress_technology(user, technology)
+    percentage = technology.themes.reduce(0) do |progress, theme|
+      if theme.survey_id
+        progress += progress_theme(user, theme.survey_id)
+      end
+      progress
     end
+    percentage.fdiv(technology.themes.count)
   end
 end
